ccb0ad01f1bdd7f579ab3ae3e1b2bd955d19f5ed
OB-ONE <last_life@hotmail.com>
2020-07-17 18:32:48 +0700

    modified:   index.php

--- index.php
+++ index.php
@@ -1,3 +1,58 @@
-<?php 
-echo 'OK';
-echo 'TEST';
+<?php
+/**
+ * Use for return easy answer.
+ */
+
+require_once('./vendor/autoload.php');
+
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
+$channel_token = 'LCbmoMjbF2nRv/Otz/dWhlTDAFIEDWQhmQrcAwn2xz9wEm8/OZcznhNKgVt6pHAkixKM/w4CbrVXb+AVb+uUbQ4sEhsCliL9/TaY57smH118ZKmo+OiV/biDXkJzzeFq1zGtFu12OQslMNbkSeEYywdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'b3ae34bda8a0a53b84a4ab8d11dc3106';
+
+// Get message from Line API
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+
+if (!is_null($events['events'])) {
+
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+    
+        // Line API send a lot of event type, we interested in message only.
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+
+            // Get replyToken
+            $replyToken = $event['replyToken'];
+
+            switch($event['message']['text']) {
+                
+                case 'tel':
+                    $respMessage = '089-5124512';
+                    break;
+                case 'address':
+                    $respMessage = '99/451 Muang Nonthaburi';
+                    break;
+                case 'boss':
+                    $respMessage = '089-2541545';
+                    break;
+                case 'idcard':
+                    $respMessage = '5845122451245';
+                    break;
+                default:
+                    break;
+            }
+
+            $httpClient = new CurlHTTPClient($channel_token);
+            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+
+            $textMessageBuilder = new TextMessageBuilder($respMessage);
+            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
+		}
+	}
+}
+
+echo "OK";
