bf4bda55835f8c75fefaa49779233a59e5c4a37a
OB-ONE <last_life@hotmail.com>
2020-07-19 16:49:21 +0700

    modified:   index.php

--- index.php
+++ index.php
@@ -48,7 +48,8 @@ if (!is_null($events['events'])) {
                             $result = $connection->query($sql);
 							if($result !== false && $result->rowCount() >0) 
 								{
-									$row = $result -> fetch_array();
+									//$row = $result -> fetch_array();
+									$row = pg_fetch_array($result, NULL, PGSQL_ASSOC);
 										//$amount = $result->rowCount();
 										//$respMessage =("%s (%s)\n", $row[0], $row[1]);
 										$respMessage = $row['province'];
