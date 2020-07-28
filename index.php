92242706d95605e3e66197635106a6856c09cfe6
OB-ONE <last_life@hotmail.com>
2020-07-28 17:57:32 +0700

    modified:   index.php

--- index.php
+++ index.php
@@ -22,13 +22,13 @@
 
 //$channel_secret = 'b3ae34bda8a0a53b84a4ab8d11dc3106';
 
-	$LINEProfileDatas['url'] = "https://api.line.me/v2/bot/profile/".$userId;
+	/*$LINEProfileDatas['url'] = "https://api.line.me/v2/bot/profile/".$userId;
   	$LINEProfileDatas['token'] = $token;
 
   	$resultsLineProfile = getLINEProfile($LINEProfileDatas);
 
   	$LINEUserProfile = json_decode($resultsLineProfile['message'],true);
-  	$displayName = $LINEUserProfile['displayName'];
+  	$displayName = $LINEUserProfile['displayName'];*/
 
 	/*
 	  * We need to get a Google_Client object first to handle auth and api calls, etc.
@@ -44,7 +44,7 @@
     $spreadsheetId = "1KM7Ldb6BjFOkwwQtKHcZNyuQTrsTa0qIcaY-3dlmdx0";
 
     // updateData($spreadsheetId,$service);
-    insertData($spreadsheetId,$service,$displayName);
+    //insertData($spreadsheetId,$service,$displayName);
 
 	/*function insertData($spreadsheetId,$service,$displayName)
     {
